5770bf3af01117a7dfe8faebc585860a45b7a42d
pnosong <pnosong545@naver.com>
2023-02-06 01:14:02 +0900

    01_babbling.js finish

---
+++ 01_ babbling.js
@@ -0,0 +1,9 @@
+function solution(babbling){
+    const possible_pronunciations = /aya|ye|woo|ma/g;
+
+    return answer = babbling.map(i => i.replace(possible_pronunciations, 1)).filter(i => !isNaN(i)).length;
+}
+
+console.log(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]));
+
+// 테스트 케이스 17번 오류 발생
